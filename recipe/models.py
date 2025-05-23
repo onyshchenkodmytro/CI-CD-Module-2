77eb53707edb54f925a4e5929e8aef6f29f55c54
onyshchenkodmytro <dimonchakus@gmail.com>
2025-05-23 17:17:32 +0300

    Categories and Recipe

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+    def __iter__(self):
+        yield self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
