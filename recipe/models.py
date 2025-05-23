d862d2f20a7e698e5af763b75d50203a5a5e8284
onyshchenkodmytro <dimonchakus@gmail.com>
2025-05-23 16:52:17 +0300

    Add Recipe model

--- recipe/models.py
+++ recipe/models.py
@@ -9,4 +9,15 @@ class Category(models.Model):
     def __iter__(self):
         yield self.name
 
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
