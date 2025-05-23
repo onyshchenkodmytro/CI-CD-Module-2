829f6040d8b5f3d16d8eb76112b423954d92774a
onyshchenkodmytro <dimonchakus@gmail.com>
2025-05-23 16:47:06 +0300

    Add Category model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def __iter__(self):
+        yield self.name
+
 
